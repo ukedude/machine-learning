cc4c5b7e1ee62b3cb3e3069ce7f30d50cbeae601
Steve Jines <Steve Jines>
2017-04-05 18:10:00 -0700

    Updated q learning code to use proper weighting. Rerean simulation and anlyzed results.

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -168,7 +168,7 @@ class LearningAgent(Agent):
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
         if self.learning:
-            self.Q[state][action] = (1 - self.alpha ) * self.Q[state][action] + self.alpha * (reward - self.Q[state][action])
+            self.Q[state][action] = (1 - self.alpha ) * self.Q[state][action] + self.alpha * reward 
 
     
         return
